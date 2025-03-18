f2479812bae0658f986ae6b38093b37734bd55c9
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-18 10:23:39 +0800

    Initial Commit

---
+++ Activity 2/Activity_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_2App.swift
+//  Activity 2
+//
+//  Created by John Joaquin Valdez on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Activity_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
